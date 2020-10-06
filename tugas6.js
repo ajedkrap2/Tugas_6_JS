2715432ce8a07db6cc812352007f1031477bfcec
ajedkrap <mr.thedja@gmail.com>
2020-10-07 00:19:11 +0700

    submit

---
+++ tugas6.js
@@ -0,0 +1,14 @@
+function tugas6(){
+  var x = 1;
+  while (x <= 20){
+    if (x % 2 === 0){
+      console.log("Genap");
+    }
+    else {
+      console.log(x);
+    }
+    x++;
+  }
+}
+
+tugas6();
